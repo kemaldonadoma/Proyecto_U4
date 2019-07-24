25c7a18834c55ff8afb9fdfa7fad2ae2575af38d
Roberto Aguilar <33473888+aguilar2503@users.noreply.github.com>
2019-07-24 15:26:21 -0600

    commit crear usuario FrontEnd funcional

--- ProyectoU4/src/app/crear-usuario/crear-usuario.component.ts
+++ ProyectoU4/src/app/crear-usuario/crear-usuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../usuario.service';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {Router} from '@angular/router';
+import { CuentaService } from '../cuenta.service';
 
 @Component({
   selector: 'app-crear-usuario',
@@ -7,7 +11,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearUsuarioComponent implements OnInit {
 
-  constructor() { }
+  formUsuario;
+  public usuarios;
+
+
+  constructor( private apiusuario: UsuarioService, private formBuilder: FormBuilder, private router:Router, private cuentaService: CuentaService) {
+
+    this.createForm();
+
+   }
+
+   createForm(){
+    this.formUsuario = this.formBuilder.group({
+      nombre:['',[Validators.required,
+        Validators.minLength(4)]],
+      email:['',[Validators.required,
+        Validators.minLength(4)]],
+      password:['',[Validators.required,
+        Validators.minLength(4)]],
+      total:['']
+    });
+  }
+
+  guardarUsuario(usuarioToSave) {
+    console.log(usuarioToSave);
+  this.apiusuario.crearUsuario(usuarioToSave)
+    .subscribe(
+      (usr)=> {
+      
+        console.log(usr);
+        this.iniciaCuenta(usuarioToSave.total, usr);
+        console.log(usuarioToSave.total);
+        /* this.apiusuario.findAll().subscribe(
+          (usuario_result)=>{
+            this.usuarios = usuario_result;
+            console.log(this.usuarios);
+          },
+          (err)=>{
+            console.log(err);
+            this.usuarios = err;
+          }
+        );
+        this.createForm(); */
+        this.router.navigate(['']);
+
+
+      },
+
+      (err)=>{
+        console.log(err);
+      }
+    ); 
+  }
+
+  iniciaCuenta(total,usr){
+
+      this.cuentaService.inicializarCuenta({total:total},usr.data._id).subscribe(
+        (ini) =>{
+          console.log("se inicializo cuenta");
+        },
+        (err) =>{
+          console.log(err);
+        }
+      )
+  }
 
   ngOnInit() {
   }
