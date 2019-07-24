0b5ae4240524b406820bbfc866f4873da2428fec
Roberto Aguilar <33473888+aguilar2503@users.noreply.github.com>
2019-07-23 21:16:55 -0600

    commit service

--- ProyectoU4/src/app/cuenta.service.ts
+++ ProyectoU4/src/app/cuenta.service.ts
@@ -36,36 +36,29 @@ export class CuentaService {
   }
 
   public crearCategoria(id,nom,cant){
-    return this.http.put("http://167.71.202.42/api/cuentas/"+id+"/categorias/crear/"+nom+cant,null);
+    return this.http.put("http://167.71.202.42/api/cuentas/"+id+"/categorias/crear/"+nom+"/"+cant,null);
   }
 
+  public eliminarCategoria(id,nom){
+    return this.http.put("http://167.71.202.42/api/cuentas/"+id+"/categorias/eliminar/"+nom,null);
+  }
   
+  public agregarACategoria(desc,id,nom,cant){
+    return this.http.put("http://167.71.202.42/api/cuentas/"+id+"/categorias/agregar/"+nom+"/"+cant,desc);
+  }
 
+  public restarACategoria(desc,id,nom,cant){
+    return this.http.put("http://167.71.202.42/api/cuentas/"+id+"/categorias/restar/"+nom+"/"+cant,desc);
+  }
 
+  public consultaMovCat(id,nom){
+    return this.http.get("http://167.71.202.42/api/cuentas/"+id+"/categorias/movimientos/"+nom);
+  }
+
+  public todosMovimientos(id){
+    return this.http.get("http://167.71.202.42/api/cuentas/"+id+"/categorias/movimientos");
+  }
 /*  
-    ////////////////////categorias
-
-
-    //en la url se manda el parametro del nombre de la categoria a eliminar
-    router.put('/:userId/categorias/eliminar/:nom', (req, res) =>
-    cuentaCtrl.eliminarCategoria(req, res));
-
-     //se manda en el url la el nombre de la categoria y cantidad a agregar 
-    router.put('/:userId/categorias/agregar/:nom/:cant', (req, res) =>
-    cuentaCtrl.agregarCategoria(req, res));
-
-     //se manda en el url la el nombre de la categoria y cantidad a restar 
-    router.put('/:userId/categorias/restar/:nom/:cant', (req, res) =>
-    cuentaCtrl.restarCategoria(req, res));
-    
-    //se manda en la url el nombre de la categoria de la cual se van a consultar los movimientos
-    router.get('/:userId/categorias/movimientos/:nom', (req, res) =>
-    cuentaCtrl.consultarMovCat(req, res));
-
-    // se solicitan todos los movimientos  de CATEGORIAS
-    router.get('/:userId/categorias/movimientos', (req, res) =>
-    cuentaCtrl.consultarMov(req, res));
-
-
+    ////////////////////categoria
    */
 }
