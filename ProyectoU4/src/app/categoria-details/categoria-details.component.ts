80172e6eb35df643ed9468759f5cdc60e9ce46c8
aguilar2503 <roberto_.aguilar@hotmail.com>
2019-07-24 18:06:32 -0600

    commit agregar y restar categoria

--- ProyectoU4/src/app/categoria-details/categoria-details.component.ts
+++ ProyectoU4/src/app/categoria-details/categoria-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {Router , ActivatedRoute} from '@angular/router';
+import { CuentaService } from '../cuenta.service';
+
 
 @Component({
   selector: 'app-categoria-details',
@@ -7,7 +11,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaDetailsComponent implements OnInit {
 
-  constructor() { }
+  formCategoria;
+  public tip = 1;
+
+  constructor(private formBuilder: FormBuilder, private router:Router, private cuentaService: CuentaService, private activaroute: ActivatedRoute) {
+
+    this.createForm();
+   }
+
+   
+   createForm(){
+    this.formCategoria = this.formBuilder.group({
+      monto:[''],
+      agregar:[''],
+      description:['']
+    });
+  }
+
+  guardarMovimiento(movimientoToSave) {
+    console.log(movimientoToSave);
+    this.activaroute.params.subscribe(
+      (params) => {
+        if(this.tip == 1 ){
+          this.cuentaService.agregarACategoria({desc:movimientoToSave.description},params.id,params.nom,movimientoToSave.monto).subscribe(
+            (a) =>{
+              console.log("se agrego correctamente");
+            },
+            (b) => {
+              console.log("algo salio mal");
+            }
+          )
+    
+        }else{
+          this.cuentaService.restarACategoria(movimientoToSave.description,params.id,params.nom,movimientoToSave.monto).subscribe(
+            (a) =>{
+              console.log("se resto correctamente");
+            },
+            (b) => {
+              console.log("algo salio mal");
+            }
+          )
+    
+        }
+      },
+      (err) => {
+
+      }
+
+    )
+  /* this.cuentaService.agregarACategoria(usuarioToSave)
+    .subscribe(
+      (usr)=> {
+      
+        console.log(usr);
+        this.iniciaCuenta(usuarioToSave.total, usr);
+        console.log(usuarioToSave.total);
+        
+        this.router.navigate(['']);
+
+
+      },
+
+      (err)=>{
+        console.log(err);
+      }
+    );  */
+  }
+
+  tipo(num){
+    this.tip = num;
+    console.log(this.tip);
+  }
 
   ngOnInit() {
   }
