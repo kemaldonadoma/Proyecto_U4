06952be15204133f03a9bb31a0376f84dc607551
AlondraZepeda <alanzepedaam@ittepic.edu.mx>
2019-07-25 00:26:34 -0600

    movimientos-categorias + aun faltan cambios

--- ProyectoU4/src/app/movimientos-categorias/movimientos-categorias.component.ts
+++ ProyectoU4/src/app/movimientos-categorias/movimientos-categorias.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CuentaService } from '../cuenta.service';
+//import { FormGroup, FormControl, FormBuilder, Validators,FormsModule } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movimientos-categorias',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
 //  styleUrls: ['./movimientos-categorias.component.css']
 })
 export class MovimientosCategoriasComponent implements OnInit {
+  categorias:any = [];
+  categoria;
 
-  constructor() { }
+  constructor(private cuentaservice: CuentaService, private activatedRoute: ActivatedRoute, private router:Router) {
 
-  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(
+      (params)=>{
+        this.cuentaservice
+        .consultaMovimiento(params.get('id'))
+        .subscribe((result)=>{
+      this.categorias = result["movimientos"];
+    },
+     (err)=>{ 
+       console.log(err); 
+       this.categorias = [];
+      });
+
+    });
   }
 
+  back(){
+    this.activatedRoute.params.subscribe(
+      (params)=>{
+        this.router.navigate(['/',params.id])
+      },
+      (e)=>{
+
+      }
+    )
+  }
+
+  ngOnInit() { }
 }
