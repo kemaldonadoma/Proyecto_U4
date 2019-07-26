e529c21f477fdd3ab0a625e6bee7929d7e510b7f
AlondraZepeda <alanzepedaam@ittepic.edu.mx>
2019-07-25 21:43:51 -0600

    fix: errores

--- ProyectoU4/src/app/movimientos-categorias/movimientos-categorias.component.ts
+++ ProyectoU4/src/app/movimientos-categorias/movimientos-categorias.component.ts
@@ -1,67 +1,139 @@
 import { Component, OnInit } from '@angular/core';
-import { CuentaService } from '../cuenta.service';
-//import { FormGroup, FormControl, FormBuilder, Validators,FormsModule } from '@angular/forms';
-
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-
+import { UsuarioService } from "../usuario.service";
+import { CuentaService } from "../cuenta.service";
+import {FormGroup,FormControl,FormBuilder,Validators} from "@angular/forms";
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-movimientos-categorias',
   templateUrl: './movimientos-categorias.component.html'
   //  styleUrls: ['./movimientos-categorias.component.css']
 })
 export class MovimientosCategoriasComponent implements OnInit {
+  categorias;
+  total;
+  usuario;
+  formTotal;
+  public tip = 1;
 
-  categorias: any = [];
-  categoria;
+  constructor(  private activaroute: ActivatedRoute, private apiCuenta:CuentaService,private router:Router,private apiUser:UsuarioService,private formBuilder:FormBuilder) {
+   
+    this.createForm();
+    //obtener nombre de usuario
+    this.activaroute.paramMap.subscribe(
+      (params)=>{
+        this.apiUser.findById(params.get('id')).subscribe(
+          (cat_result)=>{
+            this.asigNom(cat_result);
+          },
+          (err)=>{
+            console.log(err);
+            this.categorias = err;
+          }
+        );
+        
+      },
+      ()=>{}
+    );
 
-  constructor(private cuentaservice: CuentaService, private activatedRoute: ActivatedRoute, private router: Router) {
 
-    this.activatedRoute.paramMap.subscribe(
-      (params) => {
-        this.cuentaservice
-          .consultaMovimiento(params.get('id'))
-          .subscribe((result) => {
-            console.log(result);
-            console.log(params.get('id'));
-          this.categorias = result.movimientos;
-            console.log("Successful");
+    //obtener categorias
+    this.activaroute.paramMap.subscribe(
+      (params)=>{
+        this.apiCuenta.consultaCategorias(params.get('id')).subscribe(
+          (cat_result)=>{
+            
+            this.asigCat(cat_result);
+            console.log("Test de categorias sobre id");
             console.log(this.categorias);
           },
-            (err) => {
-              console.log(err);
-              this.categorias = [];
-            });
-
-      });
-  }
-
-  refresh() {
-    this.activatedRoute.params.subscribe(
-      (params) => {
-        this.router.navigate(['/', params.id, 'movimientos-categoria'])
+          (err)=>{
+            console.log(err);
+            this.categorias = err;
+          }
+        );
+        
       },
-      (e) => {
+      ()=>{}
+    );
+   }
 
-      }
-
-    )
+  asigCat(cat){
+    console.log(cat);
+    this.categorias = cat.data[0].categorias;
+    this.total = cat.data[0].total;
+    
+  }
+  asigNom(us){
+    this.usuario = us.data.nombre;
   }
 
-  back() {
-    this.activatedRoute.params.subscribe(
-      (params) => {
-        this.router.navigate(['/', params.id])
-      },
-      (e) => {
-
-      }
-    )
+  ngOnInit() {
   }
 
-  load() {
+  load(){
     location.reload();
   }
 
-  ngOnInit() { }
+createForm(){
+    this.formTotal = this.formBuilder.group({
+      monto:[''],
+      agregar:[''],
+      description:['']
+    });
+  }
+
+  guardarMovimiento(movimientoToSave) {
+    console.log(movimientoToSave);
+    this.activaroute.params.subscribe(
+
+      (params) => {
+        if(this.tip == 1 ){
+          this.apiCuenta.agregarTotal(params.id,movimientoToSave.monto,{desc:movimientoToSave.description}).subscribe(
+            (a) =>{
+              console.log("se agrego correctamente a total");
+              this.load();
+            },
+            (b) => {
+              console.log("algo salio mal en agregar");
+            }
+          )
+    
+        }else{
+          this.apiCuenta.restarTotal(params.id,movimientoToSave.monto,{desc:movimientoToSave.description}).subscribe(
+            (a) =>{
+              console.log("se resto correctamente a total");
+              this.load();
+            },
+            (b) => {
+              console.log("algo salio mal en insertar");
+            }
+          )
+    
+        }
+      },
+      (err) => {
+
+      }
+
+    )
+
+  }
+  regresar(){
+    this.activaroute.params.subscribe(
+      (params)=>{
+        this.router.navigate(['/',params.id])
+      },
+      (e)=>{
+
+      }
+
+    )
+  }
+
+  tipo(num){
+    this.tip = num;
+    console.log(this.tip);
+  }
+
+  
 }
